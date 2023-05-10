99e0abc87e8102f43fcaea54a4a8745265cd2100
Seow Alex <seowalex@hotmail.com>
2023-05-10 17:04:23 +0800

    Add Shoko widget

--- src/widgets/components.js
+++ src/widgets/components.js
@@ -110,6 +110,7 @@ const components = {
   rutorrent: dynamic(() => import("./rutorrent/component")),
   sabnzbd: dynamic(() => import("./sabnzbd/component")),
   scrutiny: dynamic(() => import("./scrutiny/component")),
+  shoko: dynamic(() => import("./shoko/component")),
   sonarr: dynamic(() => import("./sonarr/component")),
   speedtest: dynamic(() => import("./speedtest/component")),
   spoolman: dynamic(() => import("./spoolman/component")),
--- src/widgets/widgets.js
+++ src/widgets/widgets.js
@@ -101,6 +101,7 @@ import readarr from "./readarr/widget";
 import rutorrent from "./rutorrent/widget";
 import sabnzbd from "./sabnzbd/widget";
 import scrutiny from "./scrutiny/widget";
+import shoko from "./shoko/widget";
 import sonarr from "./sonarr/widget";
 import speedtest from "./speedtest/widget";
 import spoolman from "./spoolman/widget";
@@ -238,6 +239,7 @@ const widgets = {
   rutorrent,
   sabnzbd,
   scrutiny,
+  shoko,
   sonarr,
   speedtest,
   spoolman,

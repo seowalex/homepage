0741251de692b1afcd22c7eaf89162814c94a15c
Seow Alex <seowalex@hotmail.com>
2023-05-10 17:04:23 +0800

    Add Shoko widget

--- src/widgets/widgets.js
+++ src/widgets/widgets.js
@@ -92,6 +92,7 @@ import readarr from "./readarr/widget";
 import rutorrent from "./rutorrent/widget";
 import sabnzbd from "./sabnzbd/widget";
 import scrutiny from "./scrutiny/widget";
+import shoko from "./shoko/widget";
 import sonarr from "./sonarr/widget";
 import speedtest from "./speedtest/widget";
 import stash from "./stash/widget";
@@ -215,6 +216,7 @@ const widgets = {
   rutorrent,
   sabnzbd,
   scrutiny,
+  shoko,
   sonarr,
   speedtest,
   stash,
